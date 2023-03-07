aa3355501b82813592c75fa59862d63390c5bb45
Aduilio Silva <developer.aduilio@gmail.com>
2023-03-06 21:03:11 -0300

    Create the models
    - Create the project and the models that represents
      the business logic.

---
+++ src/test/kotlin/com/aduilio/kotlin/forum/ForumApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.aduilio.kotlin.forum
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ForumApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
